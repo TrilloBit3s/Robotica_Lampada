dfa5d1fee1d98a9bb3cdeb639573e4ab5a1dfe2b
trillobit3s <79748858+TrilloBit3s@users.noreply.github.com>
2024-08-25 15:36:37 -0300

    Add files via upload

--- Lampada/acende_led/acende_led.ino
+++ Lampada/acende_led/acende_led.ino
@@ -1,27 +1,54 @@
-    //acender lampada com app
-    #define lampada_porta  8      
+//Script para ligar dispositivos no arduino
+//Dev: Michael M.Sabino
 
-    int comando; 
-    boolean lampada = false;  
+#define lampada2_porta 7  
+#define lampada_porta  8   		
+#define ventilador_porta  12       
 
-    void setup() {  
-        pinMode(lampada_porta, OUTPUT);
-        Serial.begin(9600); 
-      } 
-      
-    void loop(){
-      if (Serial.available() > 0) {
-        comando = Serial.read();
-      
-      if (lampada) {
-      digitalWrite(lampada_porta, HIGH);
-      }
-      else {
-        digitalWrite(lampada_porta, LOW);
-        }
-       switch (comando) {
-         case 'L':lampada = true;break;
-         case 'l':lampada = false;break;
-         }
-       }
-    }
+int comando;
+boolean lampada = false;
+boolean lampada2 = false;
+boolean ventilador = false;
+
+void setup(){  
+	pinMode(lampada_porta, OUTPUT);
+	pinMode(lampada2_porta, OUTPUT);
+	pinMode(ventilador_porta, OUTPUT);
+	Serial.begin(9600); 
+} 
+  
+void loop(){
+  if(Serial.available() > 0){
+		comando = Serial.read();
+
+	if(lampada){
+		digitalWrite(lampada_porta, HIGH);
+	}else{
+		digitalWrite(lampada_porta, LOW);
+	}
+		switch (comando){
+			case 'l':lampada = true;break;
+			case 'L':lampada = false;break;
+		}
+	}
+    
+	if(ventilador){
+		digitalWrite(ventilador_porta, HIGH);
+	}else{
+		digitalWrite(ventilador_porta, LOW);
+	}
+		switch (comando){
+			case 'v':ventilador = true;break;
+			case 'V':ventilador = false;break;
+		} 
+
+	if(lampada2){
+		digitalWrite(lampada2_porta, HIGH);
+	}else{
+		digitalWrite(lampada2_porta, LOW);
+	}
+		switch (comando){
+			case 'm':lampada2 = true;break;
+			case 'M':lampada2 = false;break;
+		}      
+}
